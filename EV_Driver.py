63dec2b922af8d7317c1f14cd1230dc84e16629b
jmas14 <jmas14@gcloud.ua.es>
2025-10-16 18:05:37 +0200

    add algunos cambios en EV_Driver.py

--- EV_Driver.py
+++ EV_Driver.py
@@ -1,20 +1,26 @@
 import socket
 import sys
+import time
+from Cliente import Cliente
+from Servicio import Servicio
+
 
 HEADER = 64
 PORT = 5050
 FORMAT = 'utf-8'
 FIN = "FIN"
+DELAYS = 4  # segundos entre solicitudes
 
-def send(msg):
+def send(sock, msg):
     message = msg.encode(FORMAT)
     msg_length = len(message)
     send_length = str(msg_length).encode(FORMAT)
     send_length += b' ' * (HEADER - len(send_length))
-    client.send(send_length)
-    client.send(message)
+    sock.send(send_length)
+    sock.send(message)
 
-DELAYS = 4  # segundos entre solicitudes
+def recv(sock):
+    return sock.recv(2048).decode(FORMAT)
 
 def SelectServices(sock, cliente):
     """
@@ -23,56 +29,59 @@ def SelectServices(sock, cliente):
     de la Central para cada solicitud. Escribe 0 o FIN para salir.
     """
     print("\n=== Servicios disponibles ===")
+    print("0 - salir")
     print("1 - Registro de usuario")
     print("2 - Solicitar autorización de suministro")
     print("3 - Consultar estado de un CP")
-    print("Cualquier otra tecla salir")
     print("=============================\n")
 
     while True:
         op = input("Selecciona servicio (1, 2 o 3): ").strip()
-
-        # Validar opción -> crear Servicio
-        try:
-            n = int(op)
-            servicio = Servicio(n)
+        n = int(op)
+        
+        if n==0:
+            break
+        else:
+            # Validar opción -> crear Servicio
+            try:
+                servicio = Servicio(n)
             
-            # Preparar y enviar mensaje según servicio
-            if servicio.get_tipo() == Servicio.Tipo.REGISTRO:
-                msg = f"REGISTRO|{cliente.getID()}"
-                print("[DRV]: ", msg)
-                send(sock, msg)
-                resp = recv(sock)
-                print("[CENTRAL]: ", resp)
+                # Preparar y enviar mensaje según servicio
+                if servicio.get_tipo() == Servicio.Tipo.REGISTRO:
+                    msg = f"REGISTRO|{cliente.getID()}" 
+                    print("[DRV]: ", msg)
+                    send(sock, msg)
+                    resp = recv(sock)
+                    print("[CENTRAL]: ", resp)
 
-            elif servicio.get_tipo() == Servicio.Tipo.AUTORIZACION:
-                cp_id = input("Introduce el ID del CP: ").strip()
-                if not cp_id:
-                    print("CP_ID vacío, cancelo solicitud.")
-                    continue
-                msg = f"AUTORIZACION|{cliente.getID()}|{cp_id}"
-                print("[DRV] ", msg)
-                send(sock, msg)
-                resp = recv(sock)
-                print("[CENTRAL] ", resp)
+                elif servicio.get_tipo() == Servicio.Tipo.AUTORIZACION:
+                    cp_id = input("Introduce el ID del CP: ").strip()
+                    if not cp_id:
+                        print("CP_ID vacío, cancelo solicitud.")
+                        continue
+                    msg = f"AUTORIZACION|{cliente.getID()}|{cp_id}" 
+                    print("[DRV] ", msg)
+                    send(sock, msg)
+                    resp = recv(sock)
+                    print("[CENTRAL] ", resp)
 
-            else:
-                cp_id = input("Introduce el ID del CP: ").strip()
-                if not cp_id:
-                    print("CP_ID vacío, cancelo consulta.")
-                    continue
-                msg = f"STATUS|{cp_id}"
-                print("[DRV] ", msg)
-                send(sock, msg)
-                resp = recv(sock)
-                print("[CENTRAL] ", resp)
+                else:
+                    cp_id = input("Introduce el ID del CP: ").strip()
+                    if not cp_id:
+                        print("CP_ID vacío, cancelo consulta.")
+                        continue
+                    msg = f"ESTADO|{cp_id}" 
+                    print("[DRV] ", msg)
+                    send(sock, msg)
+                    resp = recv(sock)
+                    print("[CENTRAL] ", resp)
             
-        except ValueError:
-            print("Opción inválida. Usa 1, 2 o 3.")
-            continue
+            except ValueError:
+                print("Opción inválida. Usa 0, 1, 2 o 3.")
+                continue
 
-        # Espera obligatoria tras cada respuesta (éxito o error)
-        time.sleep(DELAYS)
+            # Espera obligatoria tras cada respuesta (éxito o error)
+            time.sleep(DELAYS)
 
 ########## MAIN ##########
 
@@ -92,7 +101,7 @@ def main():
     sock.connect(addr)
     print(f"[EV_Driver] Conectado a CENTRAL en {addr}")
     
-    SelectServices(sock, cliente)
+    SelectServices(sock, driver)
 
     print("Cerrando sesión…")
     send(sock, FIN)
