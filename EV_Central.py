1edf61704ed0591602414027204266c79ef99a20
jmas14 <jmas14@gcloud.ua.es>
2025-10-29 10:24:19 +0100

    drop funciones repetidas

--- EV_Central.py
+++ EV_Central.py
@@ -18,58 +18,7 @@ CPs_AUX = []
 CPs_IDX = []
 CUSTOMER_IDX = []
 TOPIC_REQUESTS = "driver-to-central"
-<<<<<<< HEAD
-=======
 
-def handle_client(conn, addr):
-    print(f"[NUEVA CONEXION] {addr} connected.")
-
-    connected = True
-    while connected:
-        msg_length = conn.recv(HEADER).decode(FORMAT)
-        if msg_length:
-            msg_length = int(msg_length)
-            msg = conn.recv(msg_length).decode(FORMAT)
-            if msg == FIN:
-                connected = False
-            else:
-                parts = msg.split("|") # split quita los separadores y convierte el mensaje en una lista 
-                if parts[0] == "driver":
-                    # El mensaje viene de un driver
-                    resp = attendToDriver(parts[1], parts[2]) # peticion, cliente
-                elif parts[0] == "monitor":
-                    conectado = "1"
-                    conn.send(conectado.encode(FORMAT))
-                    resp = attendToMonitor(parts[1],parts[2],parts[3]) # LLAMAR A LA FUNCION attendToMonitor
-                else:
-                    resp = "ERROR: peticion de origen desconocido"
-            print(f" He recibido del cliente [{addr}] el mensaje: {msg}")
-            #conn.send(f"HOLA CLIENTE: He recibido tu mensaje: {msg} ".encode(FORMAT))
-            conn.send(resp.encode(FORMAT))
-    print("ADIOS. TE ESPERO EN OTRA OCASION")
-    conn.close()
-    
-        
-
-def start(server):
-    server.listen()
-    print(f"[LISTENING] Servidor a la escucha en {SERVER}")
-    CONEX_ACTIVAS = threading.active_count()-1
-    print(CONEX_ACTIVAS)
-    while True:
-        conn, addr = server.accept()
-        CONEX_ACTIVAS = threading.active_count()
-        if (CONEX_ACTIVAS <= MAX_CONEXIONES): 
-            thread = threading.Thread(target=handle_client, args=(conn, addr))
-            thread.start()
-            print(f"[CONEXIONES ACTIVAS] {CONEX_ACTIVAS}")
-            print("CONEXIONES RESTANTES PARA CERRAR EL SERVICIO", MAX_CONEXIONES-CONEX_ACTIVAS)
-        else:
-            print("OOppsss... DEMASIADAS CONEXIONES. ESPERANDO A QUE ALGUIEN SE VAYA")
-            conn.send("OOppsss... DEMASIADAS CONEXIONES. Tendrás que esperar a que alguien se vaya".encode(FORMAT))
-            conn.close()
-            CONEX_ACTUALES = threading.active_count()-1
->>>>>>> 69c97b67003fd51a312f77be1b12a9ae2a59969e
         
 ########################## MONITOR ######################
             
